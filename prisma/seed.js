bba6ecbbf80117bb1ef0996e9c23ede40349af36
MagdielAndres <magdiel.vampire@gmail.com>
2022-05-23 13:46:27 -0500

    Agregando datos en la tabla missioncomander

--- prisma/seed.js
+++ prisma/seed.js
@@ -3,53 +3,83 @@ const prisma = new PrismaClient();
 
 (async function main() {
   try {
-    const woopa = await prisma.explorer.upsert({
-      where: { name: 'Woopa' },
+    // const woopa = await prisma.explorer.upsert({
+    //   where: { name: 'Woopa' },
+    //   update: {},
+    //   create: {
+    //     name: 'Woopa',
+		// 		username: 'ajolonauta',
+		// 		mission: 'Node'
+    //   },
+    // });
+
+    // const woopa1 = await prisma.explorer.upsert({
+    //   where: { name: 'Woopa1' },
+    //   update: {},
+    //   create: {
+    //     name: 'Woopa1',
+		// 		username: 'ajolonauta1',
+		// 		mission: 'Node'
+    //   },
+    // });
+
+    // const woopa2 = await prisma.explorer.upsert({
+    //   where: { name: 'Woopa 2' },
+    //   update: {},
+    //   create: {
+    //     name: 'Woopa 2',
+		// 		username: 'ajolonauta2',
+		// 		mission: 'Java'
+    //   },
+    // });
+
+    // const woopa3 = await prisma.explorer.upsert({
+    //   where: { name: 'Woopa 3' },
+    //   update: {},
+    //   create: {
+    //     name: 'Woopa 3',
+		// 		username: 'ajolonauta3',
+		// 		mission: 'Node'
+    //   },
+    // });
+
+    // const woopa4 = await prisma.explorer.upsert({
+    //   where: { name: 'Woopa 4' },
+    //   update: {},
+    //   create: {
+    //     name: 'Woopa 4',
+		// 		username: 'ajolonauta4',
+		// 		mission: 'Node'
+    //   },
+    // });
+
+    const missionComader1 = await prisma.missionComander.upsert({
+      where: { name: 'Carlo' },
       update: {},
       create: {
-        name: 'Woopa',
-				username: 'ajolonauta',
-				mission: 'Node'
+        name: 'Carlo Gilmar',
+				username: 'Carlogilmar',
+				mainStack: 'Node'
       },
     });
 
-    const woopa1 = await prisma.explorer.upsert({
-      where: { name: 'Woopa1' },
+    const missionComader2 = await prisma.missionComander.upsert({
+      where: { name: 'Rodrigo' },
       update: {},
       create: {
-        name: 'Woopa1',
-				username: 'ajolonauta1',
-				mission: 'Node'
+        name: 'Rodrigo',
+				username: 'rodrigo',
+				mainStack: 'JavaScript'
       },
     });
 
-    const woopa2 = await prisma.explorer.upsert({
-      where: { name: 'Woopa 2' },
+    const missionComader3 = await prisma.missionComander.upsert({
+      where: { name: 'Fernanda' },
       update: {},
       create: {
-        name: 'Woopa 2',
-				username: 'ajolonauta2',
-				mission: 'Java'
-      },
-    });
-
-    const woopa3 = await prisma.explorer.upsert({
-      where: { name: 'Woopa 3' },
-      update: {},
-      create: {
-        name: 'Woopa 3',
-				username: 'ajolonauta3',
-				mission: 'Node'
-      },
-    });
-
-    const woopa4 = await prisma.explorer.upsert({
-      where: { name: 'Woopa 4' },
-      update: {},
-      create: {
-        name: 'Woopa 4',
-				username: 'ajolonauta4',
-				mission: 'Node'
+        name: 'Fernanda',
+				username: 'fernanda',
+				mainStack: 'Java'
       },
     });
 
